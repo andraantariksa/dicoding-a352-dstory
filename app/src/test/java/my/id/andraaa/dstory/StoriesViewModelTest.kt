92912421c615957eab137c4e26c90e65085cbe2e
Andra Antariksa <andra.antariksa@gmail.com>
2023-02-12 23:22:57 +0700

    Add minimal error test

--- app/src/test/java/my/id/andraaa/dstory/StoriesViewModelTest.kt
+++ app/src/test/java/my/id/andraaa/dstory/StoriesViewModelTest.kt
@@ -1,35 +1,72 @@
 package my.id.andraaa.dstory
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import my.id.andraaa.dstory.stories.data.DicodingStoryDataSourceImpl
-import my.id.andraaa.dstory.stories.data.service.DicodingStoryService
-import my.id.andraaa.dstory.stories.data.service.response.StoriesResponse
-import my.id.andraaa.dstory.stories.presentor.main.stories.StoriesViewModel
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.test.*
+import my.id.andraaa.dstory.stories.data.service.response.Story
+import my.id.andraaa.dstory.stories.presentor.main.stories.StoriesPagingAdapter
 import org.junit.Before
-import org.mockito.kotlin.doReturn
-import org.mockito.kotlin.mock
+import org.junit.Test
+
+//@ExperimentalCoroutinesApi
+//class MainDispatcherRule(
+//    private val testDispatcher: TestDispatcher = UnconfinedTestDispatcher(),
+//) : TestWatcher() {
+//    override fun starting(description: Description) {
+//        Dispatchers.setMain(testDispatcher)
+//    }
+//
+//    override fun finished(description: Description) {
+//        Dispatchers.resetMain()
+//    }
+//}
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class StoriesViewModelTest {
-    private lateinit var viewModel: StoriesViewModel
-
+    //    @get:Rule
+//    val coroutineRule = MainDispatcherRule()
     @Before
     fun setUp() {
-        val storyFactory = StoryFactory()
+        StoriesPagingAdapter(Story.DIFF_UTIL)
+    }
 
-        val dicodingStoryServiceMock = mock<DicodingStoryService> {
-            for (page in 1..2) {
-                onBlocking { getStories(page) } doReturn StoriesResponse(error = false,
-                    message = "",
-                    listStory = List(5) {
-                        storyFactory.createStory()
-                    })
-            }
-            onBlocking { getStories(3) } doReturn StoriesResponse(
-                error = false, message = "", listStory = listOf()
-            )
-        }
-        val dicodingStoryDataSource = DicodingStoryDataSourceImpl(dicodingStoryServiceMock)
-        viewModel = StoriesViewModel(dicodingStoryDataSource)
+    @Test
+    fun storiesFlow_isCorrect() = runTest {
+//        val storyFactory = StoryFactory()
+//
+//        val page1 = List(5) {
+//            storyFactory.createStory()
+//        }
+//        val page2 = List(5) {
+//            storyFactory.createStory()
+//        }
+//
+//        val dicodingStoryServiceMock = mock<DicodingStoryService> {
+//            onBlocking { getStories(1) } doReturn StoriesResponse(
+//                error = false,
+//                message = "",
+//                listStory = page1
+//            )
+//            onBlocking { getStories(2) } doReturn StoriesResponse(
+//                error = false,
+//                message = "",
+//                listStory = page2
+//            )
+//            onBlocking { getStories(3) } doReturn StoriesResponse(
+//                error = false, message = "", listStory = listOf()
+//            )
+//        }
+//        val dicodingStoryDataSource = DicodingStoryDataSourceImpl(dicodingStoryServiceMock)
+//        val pagingSource = StoriesPagingSource(dicodingStoryDataSource)
+//        val viewModel = StoriesViewModel(dicodingStoryDataSource)
+//
+//        val stories = viewModel.storiesFlow.first()
+
+//        val adapter = StoriesPagingAdapter(
+//            Story.DIFF_UTIL
+//        )
+//        adapter.submitData(stories)
+//        pagingSource.
+//        assertEquals(page1, adapter.snapshot().items)
     }
 }
